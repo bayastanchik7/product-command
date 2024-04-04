f51ff2c761b176f583a39403873bceccd2d27ab6
bayastanchik7 <bayastantursumamatov@gmail.com>
2024-04-04 16:42:34 +0600

    product

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
+import Mainheaderendnavbar from './components/MainBlockProduct/MainHeaderend/Mainheaderendnavbar'
+import Mainheadernavbar from './components/MainBlockProduct/Mainheadernavbarapp/Mainheadernavbar'
+import Mainheaderstartnavbar from './components/MainBlockProduct/Mainheaderstart/Mainheaderstartnavbar'
 
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+	return (
+		<>
+			<Mainheadernavbar />
+			<Mainheaderstartnavbar />
+			<Mainheaderendnavbar/>
+		</>
+	)
 }
 
 export default App
